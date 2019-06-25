8701c7367e668a855eb8889f3ed6fd8eda436c62
Ryuhei Mori <mori@c.titech.ac.jp>
2019-06-25 12:32:54 +0900

    revised

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -13,21 +13,16 @@ void swap(int *p, int *q){
   *q = tmp;
 }
 
-
-/*
-A[0], A[1], ..., A[n-1] の中でk+1番目に小さい値を返す関数
-ただし、Aの中身は書き換えてしまう。
-*/
 int quick_select(int A[], int n, int k){
   int i, j, pivot;
 
-// 先頭の要素をピボットとする
-  pivot = A[0];
+// 真ん中の要素をピボットとする
+  pivot = A[n/2];
+  A[n/2] = A[0];
+  A[0] = pivot;
   for(i = j = 1; i < n; i++){
     if(A[i] <= pivot){
-      int z = A[j];
-      A[j] = A[i];
-      A[i] = z;
+      swap(A+i, A+j);
       j++;
     }
   }
@@ -36,6 +31,7 @@ int quick_select(int A[], int n, int k){
   else if(j < k+1) return quick_select(A+j, n-j, k-j);
   else return quick_select(A+1, j-1, k);
 }
+
 
 int main(){
   int i;
