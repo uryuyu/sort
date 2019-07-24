efdac63106dbaca5da769a20c3030ed8cd68fa00
uryuyu <uryu.y.aa@m.titech.ac.jp>
2019-07-24 17:59:23 +0900

    a

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -15,7 +15,7 @@ void swap(int *p, int *q){
 
 int quick_select(int A[], int n, int k){
   int i, j, pivot;
-    if(n<5){
+    if(n<=5){
 // 真ん中の要素をピボットとする
   pivot = A[n/2];
   A[n/2] = A[0];
@@ -30,6 +30,7 @@ int quick_select(int A[], int n, int k){
   if(j == k+1) return pivot;
   else if(j < k+1) return quick_select(A+j, n-j, k-j);
   else return quick_select(A+1, j-1, k);
+  
 }
     else {
         int l,m;
@@ -40,26 +41,30 @@ int quick_select(int A[], int n, int k){
                 B[m] = quick_select(A+(m*5),5, 2);
             }
             else {
-                B[m] = quick_select(A+(m*5),n-(m*5),(n-(m*5)+1)/2);
+                B[m] = quick_select(A+(m*5),n-(m*5),(n-(m*5))/2);
             }
         }
-        pivot = quick_select(B, l, (l+1)/2);
-        int r,s;
+        pivot = quick_select(B, l, l/2);
+        
+        int r,s,t;
         int C[N],D[N];
-        r = s = 0;
-        while (r + s < n) {
-            if(A[r+s] <= pivot){
-                C[r] = A[r+s];
+        r = s = t = 0;
+        while (r + s + t < n) {
+            if(A[r+s+t] < pivot){
+                C[r] = A[r+s+t];
                 r ++;
             }
-            else{
-                D[s] = A[r+s];
+            else if(A[r+s+t] > pivot){
+                D[s] = A[r+s+t];
                 s ++;
             }
+            else{
+                t ++;
+            }
         }
-        if (r == k){return pivot;}
-        else if (r < k){
-            return quick_select(D,s,k-r);
+        if (r <= k && r + t > k){return pivot;}
+        else if (r + t <= k){
+            return quick_select(D,s,k-r-t);
         }
         else {
             return quick_select(C,r,k);
@@ -76,18 +81,12 @@ int main(){
   for(i=2;i<N;i++){
     A[i] = (long long int) A[i-1] * A[1] % N;
   }
-   // int E[10];
-   // for (i = 0; i< 10; i++) {
-   //     E[i] = i;
-   // }
-   // for (i = 0; i < 10; i ++) {
-   //     printf("%d\n",quick_select(E,10,i));
-   // }
+  
   for(i=0;i<N;i++){
     if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
    printf("%d th element is %d\n", i, quick_select(A, N, i));
-      
+  }
       return 0;
  }
     
-}
+
